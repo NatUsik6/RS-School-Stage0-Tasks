270f459e42b7f0c0dbaf6a895c9c67b01d8eb7e6
NatUsik6 <tashausik6@gmail.com>
2023-08-27 22:36:45 +0300

    feat: create slider about and part of slider favorites

--- library/script.js
+++ library/script.js
@@ -21,3 +21,53 @@ function disableBurgerMenuOpen() {
 
 const overlay = document.querySelector(".overlay");
 overlay.addEventListener("click", disableBurgerMenuOpen);
+
+const arrowLeft = document.querySelector(".arrow-left")
+arrowLeft.addEventListener("click", moveSliderLeft);
+const arrowRight = document.querySelector(".arrow-right")
+arrowRight.addEventListener("click", moveSliderRight);
+
+const sliderTrack = document.querySelector(".slider-track");
+const imageWidth = 475;
+let sliderPosition = 0;
+
+function moveSliderLeft() {
+    if (sliderPosition == 0) {
+        return;
+    }
+    
+    sliderPosition--;
+    moveSlider();
+}
+
+function moveSliderRight() {
+    if (sliderPosition == 4) {
+        return;
+    }
+
+    sliderPosition++;
+    moveSlider();
+}
+
+const sliderPoints = document.querySelectorAll(".separate-container");
+sliderPoints.forEach(sliderPoint => sliderPoint.addEventListener("click", changeSliderPosition));
+
+function changeSliderPosition() {
+    sliderPosition = this.dataset.pointNumber;
+    moveSlider();
+}
+
+function moveSlider() {
+    sliderTrack.style.transform = `translateX(-${imageWidth * sliderPosition}px)`;
+    sliderPoints.forEach(sliderPoint => sliderPoint.children[0].classList.remove("active"));
+    sliderPoints[sliderPosition].children[0].classList.add("active");
+}
+
+const seasonSelectors = document.querySelectorAll(".input-wrapper");
+seasonSelectors.forEach(seasonSelector => seasonSelector.addEventListener("click", changeSeasonTab));
+const seasonContainers = document.querySelectorAll(".season-container");
+
+function changeSeasonTab() {
+    seasonContainers.forEach(seasonContainer => seasonContainer.classList.add("disabled"));
+    seasonContainers[this.dataset.seasonNumber].classList.remove("disabled");
+}
