2dfc97ffc823359270d47b0269349d1afc5f7ae6
NatUsik6 <tashausik6@gmail.com>
2023-09-24 20:57:46 +0300

    feat: create audio-player without footer

--- js30-audio-player/playList.js
+++ js30-audio-player/playList.js
@@ -3,13 +3,15 @@ const playList = [
         title: "Sonne",
         author: "Rammstein",
         src: "assets/sounds/03. Sonne.mp3",
-        duration: "04:33"
+        imgSrc: "assets/rammstein_2_02.jpg",
+        duration: "04:34"
     },
     {
         title: "Mein Herz Brennt",
         author: "Rammstein",
         src: "assets/sounds/01. Mein Herz Brennt.mp3",
-        duration: "04:39"
+        imgSrc: "assets/Screen-shot-2012-12-07-at-3.02.04-PM.png",
+        duration: "04:41"
     }
 ]
 
--- js30-audio-player/script.js
+++ js30-audio-player/script.js
@@ -4,11 +4,25 @@ const playButton = document.querySelector('.play');
 playButton.addEventListener('click', playAudio);
 playButton.addEventListener('click', togglePauseButton);
 
+const songNameElement = document.querySelector(".song-name");
+const authorNameElement = document.querySelector(".author-name");
+
+const playbackBar = document.querySelector(".playback-bar");
+playbackBar.addEventListener("change", changePlaybackBarValue);
+
+const currentTimeElement = document.querySelector(".current-time");
+const songDurationElement = document.querySelector(".song-duration");
+
+const playerImage = document.querySelector(".player-image");
+const mainContainer = document.querySelector(".main-container");
+
 let isPlay = false;
 let currentSoundNumber = 0;
 
 const audio = new Audio();
+audio.addEventListener("canplay", () => playbackBar.max = Math.ceil(audio.duration));
 let timerId;
+setNewSongInfo();
 
 function playAudio() {
     clearInterval(timerId);
@@ -19,13 +33,12 @@ function playAudio() {
     }
 
     isPlay = true;
-    audio.src = playList[currentSoundNumber].src;
-    audio.currentTime = 0;
     audio.play();
-
-    playbackBar.value = 0;
-    playbackBar.max = audio.duration;
-    timerId = setInterval(() => playbackBar.value++, 1000);
+    timerId = setInterval(() => {
+        const time = new Date(0);
+        time.setSeconds(++playbackBar.value);
+        currentTimeElement.textContent = time.toLocaleTimeString().slice(3);
+    }, 1000);
 }
 
 function togglePauseButton() {
@@ -45,6 +58,7 @@ playPrevButton.addEventListener('click', togglePauseButton);
 function playPrevAudio() {
     currentSoundNumber = currentSoundNumber == 0 ? playList.length - 1 : currentSoundNumber - 1;
     isPlay = false;
+    setNewSongInfo();
     playAudio();
 }
 
@@ -55,7 +69,25 @@ playNextButton.addEventListener('click', togglePauseButton);
 function playNextAudio() {
     currentSoundNumber = currentSoundNumber == playList.length - 1 ? 0 : currentSoundNumber + 1;
     isPlay = false;
+    setNewSongInfo();
     playAudio();
 }
 
-const playbackBar = document.querySelector(".playback-bar");
+function setNewSongInfo() {
+    authorNameElement.textContent = playList[currentSoundNumber].author;
+    songNameElement.textContent = playList[currentSoundNumber].title;
+    audio.src = playList[currentSoundNumber].src;
+    playbackBar.value = 0;
+    currentTimeElement.textContent = "00:00";
+    songDurationElement.textContent = playList[currentSoundNumber].duration;
+    playerImage.style.background = `url(${playList[currentSoundNumber].imgSrc})`;
+    playerImage.style.backgroundSize = "cover";
+    mainContainer.style.backgroundImage = `url(${playList[currentSoundNumber].imgSrc})`;
+}
+
+function changePlaybackBarValue() {
+    audio.currentTime = playbackBar.value;
+    const time = new Date(0);
+    time.setSeconds(playbackBar.value);
+    currentTimeElement.textContent = time.toLocaleTimeString().slice(3);
+}
