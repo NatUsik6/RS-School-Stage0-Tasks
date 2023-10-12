0924ecd085a747d0dbc54876ad212e83805534a0
NatUsik6 <tashausik6@gmail.com>
2023-10-12 23:24:31 +0300

    feat: create moving tile and folding them

--- js30-random-game/grid.js
+++ js30-random-game/grid.js
@@ -10,11 +10,41 @@ export class Grid {
             const cell = new Cell(gridElement, i % GRID_SIZE, Math.floor (i / GRID_SIZE));
             this.cells.push(cell);
         }
+
+        this.cellsGroupedByColum = this.groupCellByColumn();
+        this.cellsGroupedByReversedColumn = this.cellsGroupedByColum.map(colum => [...colum].reverse());
+        this.cellsGroupedByRow = this.cellsGroupedByRow();
+        this.cellsGroupedByReversedRow = this.cellsGroupedByRow.map(colum => [...colum].reverse);
     }
 
     getRandomEmptyCell() {
         const emptyCells = this.cells.filter(cell => cell.isEmpty());
         const randomIndex = Math.floor(Math.random() * emptyCells.length);
         return emptyCells[randomIndex];
+    }
+
+    groupCellByColumn() {
+        // return this.cells.reduce((groupedCells, cell) => {
+        //     groupedCells[cell.x] = groupedCells[cell.x] || [];
+        //     groupedCells[cell.x][cell.y] = cell;
+        // }, []);
+
+        const groupedCells = [];
+        for (let cell of this.cells) {
+            groupedCells[cell.x] = groupedCells[cell.x] || [];
+            groupedCells[cell.x][cell.y] = cell;
+        }
+
+        return groupedCells;
+    }
+
+    cellsGroupedByRow() {
+        const groupedCells = [];
+        for (let cell of this.cells) {
+            groupedCells[cell.y] = groupedCells[cell.y] || [];
+            groupedCells[cell.y][cell.x] = cell;
+        }
+
+        return groupedCells;
     }
 }
